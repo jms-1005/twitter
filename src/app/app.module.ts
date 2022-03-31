e011e714aa087b3ff8eb2483529bea192a19c85e
John Manoah <john@vanarts.com>
2022-03-31 15:07:59 -0700

    Added a new component Like

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { TweetsComponent } from './tweets/tweets.component';
 import { RecoComponent } from './reco/reco.component';
+import { LikeComponent } from './like/like.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     TweetsComponent,
-    RecoComponent
+    RecoComponent,
+    LikeComponent
   ],
   imports: [
     BrowserModule,
